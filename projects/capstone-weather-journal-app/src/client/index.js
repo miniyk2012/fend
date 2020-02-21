aa494df282ba973edd1e7c0d6e0a23a96cf823e1
杨恺 <yangkai02@qutoutiao.net>
2020-02-21 10:39:35 +0800

    start frontend

--- projects/capstone-weather-journal-app/src/client/index.js
+++ projects/capstone-weather-journal-app/src/client/index.js
@@ -2,8 +2,6 @@ import { addListeners } from './js/listener'
 import './styles/style.scss'
 
 
-console.log(checkForName);
-
 // alert("I EXIST")
 console.log("CHANGE!!");
 
--- projects/capstone-weather-journal-app/src/server/config.js
+++ projects/capstone-weather-journal-app/src/server/config.js
@@ -1,3 +1,3 @@
 const PORT = 8085;
 
-export { PORT };
+module.exports = { PORT };
