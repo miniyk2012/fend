957b711d34c673c8fc3d1b5cf16f260bc5374962
杨恺 <yangkai02@qutoutiao.net>
2020-02-26 08:43:48 +0800

    async get destination message

--- projects/capstone-weather-journal-app/src/server/server.js
+++ projects/capstone-weather-journal-app/src/server/server.js
@@ -1,4 +1,5 @@
 const config = require('./config');
+const { geo_search, weather_forecast, pixabay_search } = require('./tools');
 
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
@@ -25,18 +26,48 @@ app.use(express.static('dist'));
 
 // Setup Server
 const port = config.PORT;
-const server = app.listen(port, listening);
+app.listen(port, listening);
 
 function listening() {
     // console.log(server);
     console.log(`running on localhost: ${port}`);
 };
 
-app.get('/all', sendData);
+app.get('/trip/search', fastSearchDestination);
 
-function sendData(request, response) {
-    console.log('get /all')
-    response.send(projectData);
+async function slowSearchDestination(request, response) {
+    country = request.query.country;
+    city = request.query.city;
+    const geo_json = await geo_search(country, city);
+    const { lat, lng } = geo_json.geonames[0];
+    const weather_json = await weather_forecast(lat, lng);
+    const pixabay_json = await pixabay_search(country, city);
+    return response.json({ geo_json, weather_json, pixabay_json });
+}
+
+async function fastSearchDestination(request, response) {
+    country = request.query.country;
+    city = request.query.city;
+
+    const promise1 = async function (country, city) {
+        const geo_json = await geo_search(country, city);
+        let weather_json;
+        if (geo_json.totalResultsCount == 0) {
+            weather_json = {
+                code: 400,
+                error: "No data"
+            };
+        } else {
+            const { lat, lng } = geo_json.geonames[0];
+            weather_json = await weather_forecast(lat, lng);
+        }
+        return { geo_json, weather_json };
+    }(country, city);
+    const promise2 = pixabay_search(country, city);
+    
+    let { geo_json, weather_json } = await promise1;
+    let pixabay_json = await promise2;
+    return response.json({ geo_json, weather_json, pixabay_json });
 }
 
 app.post('/add', addData);
@@ -46,5 +77,5 @@ function addData(request, response) {
     projectData.date = request.body.date;
     projectData.userResponse = request.body.userResponse;
     console.log('/add: projectData=', projectData)
-    response.json({"code": "0"})
+    response.json({ "code": "0" })
 }
